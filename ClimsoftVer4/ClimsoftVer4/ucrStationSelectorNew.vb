5135bcbebe2075492bcecaf58170b2565526f0ff
ivanluv <luvtheart93@gmail.com>
2020-03-10 13:18:48 +0300

    Removing errors

--- ClimsoftVer4/ClimsoftVer4/ucrStationSelectorNew.vb
+++ ClimsoftVer4/ClimsoftVer4/ucrStationSelectorNew.vb
@@ -1,51 +1,14 @@
 ﻿Public Class ucrStationSelectorNew
     '
     Public Overrides Sub PopulateControl()
-        bSuppressChangedEvents = True
-        MyBase.PopulateControl()
-        If dtbRecords.Rows.Count > 0 Then
-            cboValues.ValueMember = strStationId
-            'TODO 
-            'what if there were no records on the first load.Then there are records later
-            If bFirstLoad Then
-                SetViewTypeAsStations()
-            End If
-        Else
-            cboValues.DataSource = Nothing
-        End If
-        bSuppressChangedEvents = False
-        'OnevtValueChanged(Me, Nothing)
+        'Fill the selector combo box with the list of required values ie station ID or station Name
     End Sub
 
     Public Overrides Function ValidateValue() As Boolean
-        Dim bValid As Boolean = False
-        bValid = MyBase.ValidateValue()
-
-        If Not bValid Then
-            If Not String.IsNullOrEmpty(cboValues.ValueMember) Then
-                For Each rTemp As DataRow In dtbRecords.Rows
-                    If rTemp.Item(cboValues.ValueMember).ToString = cboValues.Text Then
-                        bValid = True
-                        Exit For
-                    End If
-                Next
-            End If
-        End If
-
-        SetBackColor(If(bValid, clValidColor, clInValidColor))
-        Return bValid
+        'TODO.
     End Function
     Private Sub cboValues_Leave(sender As Object, e As EventArgs) Handles cboValues.Leave
-        If Not cboValues.DisplayMember = strStationId Then
-            If IsNumeric(cboValues.Text) Then
-                If ValidateValue() Then
-                    Dim bChangedEvents As Boolean = Me.bSuppressChangedEvents
-                    bSuppressChangedEvents = True
-                    SetValue(cboValues.Text)
-                    bSuppressChangedEvents = bChangedEvents
-                End If
-            End If
-        End If
+        'TODO
     End Sub
     Private Sub cmsStationIDs_Click(sender As Object, e As EventArgs) Handles cmsStationIDs.Click
         SetViewTypeAsIDs()
@@ -71,27 +34,29 @@
     End Sub
 
     Public Sub SetViewTypeAsStations()
-        SetDisplayMember(strStationName)
+        'TODO. Refer to the SetDisplayMember(),in UcrComboBoxNew,while passing in the station names to get the list of specified IDS
     End Sub
 
     Public Sub SetViewTypeAsIDs()
-        SetDisplayMember(strStationId)
+        'TODO. Refer to the SetDisplayMember(),in UcrComboBoxNew,while passing in the station ID to get the list of specified IDS
     End Sub
 
     Public Sub SetViewTypeAsIDsAndStations()
-        SetDisplayMember(strIDsAndStations)
+        'TODO. Refer to the SetDisplayMember(),in UcrComboBoxNew,while passing in the station ID and Station to get the list of specified IDS
     End Sub
 
     Public Sub SortByID()
-        SortBy(strStationId)
-        cmsStationSortByID.Checked = True
-        cmsStationSortyByName.Checked = False
+        'use the sortBy(),with Id as the argument,function in ucrComboboxNew to sort the Station
+        'cmsStationSortByID.Checked = True
+        'cmsStationSortyByName.Checked = False
     End Sub
 
     Public Sub SortByStationName()
-        SortBy(strStationName)
-        cmsStationSortByID.Checked = False
-        cmsStationSortyByName.Checked = True
+        'use the sortBy(),with statioName as the argument,function in ucrComboboxNew to sort the Station
+        'cmsStationSortByID.Checked = False
+        'cmsStationSortyByName.Checked = True
     End Sub
+
+
 
 End Class
