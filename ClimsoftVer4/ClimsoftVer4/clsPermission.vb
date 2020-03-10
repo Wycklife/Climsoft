2a4d331e3fe75c0d344031dac083886ee1e86ad8
patowhiz <conlooptechnologies@gmail.com>
2020-03-10 14:16:59 +0300

    more changes

---
+++ ClimsoftVer4/ClimsoftVer4/clsPermission.vb
@@ -0,0 +1,18 @@
+﻿Public Class clsPermission
+
+    Private objPermissionDataStructure As Object
+
+
+    Public Function GetPermissionTypeName(iActionTypeId As Integer) As String
+        'todo. get the permission type name from the PermissionDataStructure
+        Return ""
+    End Function
+
+    Public Function IsVisibleFormDaily2() As Boolean
+        'todo. use the datastrucre to determine if formDaily2 should be visible or not
+        Return False
+    End Function
+
+
+
+End Class
