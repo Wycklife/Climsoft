8eb57d4b285134655c9d147f9e6fae4d41475b97
ivanluv <luvtheart93@gmail.com>
2020-03-12 15:19:53 +0300

    Minor changes

--- ClimsoftVer4/ClimsoftVer4/ucrDatePickerNew.vb
+++ ClimsoftVer4/ClimsoftVer4/ucrDatePickerNew.vb
@@ -1,20 +1,25 @@
 ﻿Public Class ucrDatePickerNew
-
-
-
+    Protected Overridable Sub ucrDatePicker_Load(sender As Object, e As EventArgs) Handles Me.Load
+        If bFirstLoad Then
+            'SetDateFormat("")   Default is not format (empty).
+            bFirstLoad = False
+        End If
+    End Sub
     Public Overrides Function IsEmpty() As Boolean
         Return True 'to be overidden by the child controls
     End Function
 
     Public Overrides Sub Clear()
-
+        'TODO Have the date picker reset to its default date
     End Sub
 
     Public Overrides Function ValidateValue() As Boolean
+        'TODO Validate the value if the date text box is not empty and return true if the value is valid or false if the value is invalid
+        'TODO there should also be changing of color in the text text box when the text is valid or invalid.This can be done throught the function SetBackColor()
         Return True
     End Function
     Public Overrides Sub GetFocus()
-
+        '
     End Sub
 
     Public Overrides Sub GetValidationParametersFromDataStructure()
@@ -29,13 +34,30 @@
         'todo. writes value to the datastructure
     End Sub
 
-
-    Public Overrides Sub SetBackColor(backColor As Color)
-    End Sub
-
     Public Overrides Function GetValue(Optional objSpecification As Object = Nothing) As Object
         'gets the value of the control.
         'Possibly pass in a parameter that specifies the value to get incase the control has more than one value
         Return Nothing
     End Function
+
+    Public Sub SetDateFormat(strNewDateFormat As String)
+        'TODO set the default format of the date
+    End Sub
+    Private Sub txtDate_Leave(sender As Object, e As EventArgs) Handles txtDate.Leave
+        If ValidateValue() Then
+            SetValue(txtDate.Text)
+        End If
+        OnevtValueChanged(Me, e)
+    End Sub
+
+    Public Sub SetValueReturnTypeAsString()
+        'TODO sets the date value as a string type
+    End Sub
+
+    Public Sub SetValueReturnTypeAsDate()
+        'TODO sets the date value as a Date type
+    End Sub
+    Public Overrides Sub SetBackColor(backColor As Color)
+        txtDate.BackColor = backColor
+    End Sub
 End Class
--- ClimsoftVer4/ClimsoftVer4/ucrElementSelectorNew.vb
+++ ClimsoftVer4/ClimsoftVer4/ucrElementSelectorNew.vb
@@ -1,3 +1,35 @@
 ﻿Public Class ucrElementSelectorNew
+    Public Overrides Function ValidateValue() As Boolean
+        Dim bValid As Boolean
+        'TODO validate values while setting the back color to indicate if the value is valid or invalid
+        Return bValid
+    End Function
+
+    Public Sub SetViewTypeAsElements()
+        'TODO set view type as the element name
+    End Sub
+
+    Public Sub SetViewTypeAsIDs()
+        'TODO set the view type as the element ID
+    End Sub
+
+    Public Sub SetViewTypeAsIDsAndElements()
+        'TODO set the view type as the elements ID and element name
+    End Sub
+
+    Private Sub SortByID()
+        'TODO sort by the elemnts ID using the sortBY() function
+        'SortBy(strElementId)
+        cmsElementSortByID.Checked = True
+        cmsElementSortyByName.Checked = False
+    End Sub
+
+    Private Sub SortByElementName()
+        'TODO sort by the elemnts Name using the ,sortBY(), function
+        'SortBy(strElementId)
+        'SortBy(strElementName)
+        cmsElementSortByID.Checked = False
+        cmsElementSortyByName.Checked = True
+    End Sub
 
 End Class
