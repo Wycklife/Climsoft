7911a12245aa7e8399269c55875cf513d40bd364
patowhiz <conlooptechnologies@gmail.com>
2020-03-12 15:55:30 +0300

    creating new frmMain

--- ClimsoftVer4/ClimsoftVer4/clsPermission.vb
+++ ClimsoftVer4/ClimsoftVer4/clsPermission.vb
@@ -9,6 +9,62 @@
         Return ClsGlobals.PermissionTypes.Disabled
     End Function
 
+
+    Public Function IsMenuAdministrationDisabled() As Boolean
+        'todo.
+        Return False
+    End Function
+
+    Public Function IsMenuToolsDisabled() As Boolean
+        'todo.
+        Return False
+    End Function
+
+    Public Function IsMenuLanguageTranslationsDisabled() As Boolean
+        'todo.
+        Return False
+    End Function
+
+    Public Function IsMenuQCsDisabled() As Boolean
+        'todo.
+        Return False
+    End Function
+
+    Public Function IsMenuSettingsAwsDisabled() As Boolean
+        'todo.
+        Return False
+    End Function
+
+    Public Function IsMenuUserManagementDisabled() As Boolean
+        'todo.
+        Return False
+    End Function
+
+    Public Function IsMenuProductsDisabled() As Boolean
+        'todo.
+        Return False
+    End Function
+
+    Public Function IsMenuMetadataDisabled() As Boolean
+        'todo.
+        Return False
+    End Function
+
+    Public Function IsMenuDatatransferDisabled() As Boolean
+        'todo.
+        Return False
+    End Function
+
+    Public Function IsMenuKeyEntryDisabled() As Boolean
+        'todo.
+        Return False
+    End Function
+
+    Public Function IsMenuPaperArchiveDisabled() As Boolean
+        'todo.
+        Return False
+    End Function
+
     Public Function IsVisibleFormDaily2() As Boolean
         'todo. use the datastrucre to determine if formDaily2 should be visible or not
         Return False
---
+++ ClimsoftVer4/ClimsoftVer4/frmMainMenuNew.vb
@@ -0,0 +1,3 @@
+﻿Public Class frmMainMenuNew
+
+End Class
