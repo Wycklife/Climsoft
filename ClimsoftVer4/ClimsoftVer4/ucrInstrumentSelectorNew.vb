1c8bdc16d52202ead8064bb49e8f76a795a0ad72
ivanluv <luvtheart93@gmail.com>
2020-03-12 15:55:21 +0300

    Instrument selector modification

--- ClimsoftVer4/ClimsoftVer4/ucrInstrumentSelectorNew.vb
+++ ClimsoftVer4/ClimsoftVer4/ucrInstrumentSelectorNew.vb
@@ -1,3 +1,22 @@
 ﻿Public Class ucrInstrumentSelectorNew
+    Private ucrLinkedElement As ucrElementSelectorNew
+    Private ucrLinkedStation As ucrStationSelectorNew
 
+    Public Sub setStationLink(ucrStationSelector As ucrStationSelectorNew)
+        ucrLinkedStation = ucrStationSelector
+        AddHandler ucrLinkedStation.evtValueChanged, AddressOf StationEvtValueChanged
+    End Sub
+
+    Public Sub SetElementLink(ucrElementSelector As ucrElementSelectorNew)
+        ucrLinkedElement = ucrElementSelector
+        AddHandler ucrLinkedElement.evtValueChanged, AddressOf ElementEvtValueChanged
+    End Sub
+    Private Sub ElementEvtValueChanged()
+        'TODO. Filter out the elements for the specific Instruments when the eveent is raised
+    End Sub
+
+    Private Sub StationEvtValueChanged()
+        'TODO.Filter out the stations with the instruments
+    End Sub
 End Class
+
